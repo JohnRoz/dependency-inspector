74913c11f94d0115fbdda1583d02f5427819bf44
Jonathan <jonathan.rozenblat@gmail.com>
2025-01-10 17:12:37 +0200

    Fixed order of decorators in base class

--- pkg1/base_config.py
+++ pkg1/base_config.py
@@ -3,10 +3,11 @@ from abc import ABC, abstractmethod
 
 class BaseConfig(ABC):
     @classmethod
+    @abstractmethod
     def is_none_ok(cls) -> bool:
         return False
 
-    @abstractmethod
     @classmethod
+    @abstractmethod
     def get_required_dependencies(cls) -> set[type["BaseConfig"]]:
         pass
