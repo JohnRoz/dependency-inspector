091c8556e6e0181cee523d3c79e427fee078e7a3
Jonathan <jonathan.rozenblat@gmail.com>
2025-01-10 17:12:53 +0200

    Added subclass examples

--- pkg1/f_configs.py
+++ pkg1/f_configs.py
@@ -3,9 +3,68 @@ from abc import ABC, abstractmethod
 from pkg1.base_config import BaseConfig
 
 
-class FooConfig(BaseConfig):
+class FooConfigA(BaseConfig):
+    config: "FooConfigB"
 
-    @abstractmethod
+    def __init__(self):
+        super().__init__()
+        self.config = FooConfigB()
+
     @classmethod
     def get_required_dependencies(cls) -> set[type["BaseConfig"]]:
-        pass
+        return set()
+
+
+class FooConfigB(BaseConfig):
+    config: "FooConfigC"
+
+    def __init__(self):
+        super().__init__()
+        self.config = FooConfigC()
+
+    @classmethod
+    def get_required_dependencies(cls) -> set[type["BaseConfig"]]:
+        return set()
+
+
+class FooConfigC(BaseConfig):
+    @classmethod
+    def get_required_dependencies(cls) -> set[type["BaseConfig"]]:
+        return set()
+
+
+class FooConfigD(BaseConfig):
+    config_c: "FooConfigC"
+    config_e: "FooConfigE"
+
+    def __init__(self):
+        super().__init__()
+        self.config_c = FooConfigC()
+        self.config_e = FooConfigE()
+
+    @classmethod
+    def get_required_dependencies(cls) -> set[type["BaseConfig"]]:
+        return set()
+
+
+class FooConfigE(BaseConfig):
+    config: "FooConfigA"
+
+    def __init__(self):
+        super().__init__()
+        self.config = FooConfigA()
+
+    @classmethod
+    def is_none_ok(cls) -> bool:
+        return True
+
+    @classmethod
+    def get_required_dependencies(cls) -> set[type["BaseConfig"]]:
+        return set()
+
+
+# A -> {B}
+# B -> {C}
+# C -> {}
+# D -> {C, E}
+# E -> {A}
